c4d53da3aacdba7ab3b846a47433568f1fb3455a
PierreAND <andrieu@gmail.com>
2022-12-15 18:23:17 +0100

    reset mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,12 @@
 class UserMailer < ApplicationMailer
-  default from: "terra_green@yopmail.com"
+  default from: 'terra_green@yopmail.com'
   def welcome_email(user)
     @user = user
     @url  = 'http://localhost:3000/'
     mail(to: @user.email, subject: 'Bienvenue sur Terragreen')
+  end
+
+  def reset_password_instructions(record, token, opts = {})
+    super
   end
 end
