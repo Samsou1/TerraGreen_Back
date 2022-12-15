2db9f5061578bba6735386df96ec043febd534f2
PierreAND <andrieu@gmail.com>
2022-12-15 11:45:47 +0100

    views mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://localhost:3000/'
+    mail(to: @user.email, subject: 'Bienvenue sur Snipshare')
+  end
 end
