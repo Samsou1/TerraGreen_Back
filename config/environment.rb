60490d976a6f8f40d70ee74c1f98dc1c48cd9899
PierreAND <andrieu@gmail.com>
2022-12-15 11:22:43 +0100

    fixe config

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  user_name: apikey,
+  user_name: 'apikey',
   password: Rails.application.credentials.dig(:sendgrid, :Mailerkey),
   address: 'smtp.sendgrid.net',
   port: 587,
