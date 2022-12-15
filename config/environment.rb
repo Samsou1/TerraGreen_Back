d98301e2c8c615210629488b5dbfa0c9c3e1a2a4
PierreAND <andrieu@gmail.com>
2022-12-15 11:14:12 +0100

    last config environnement

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: Rails.application.credentials.dig(:sendgrid, :public_key),
+  password: Rails.application.credentials.dig(:sendgrid, :secret_key),
+  domain: 'snipshare.dev@gmail.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
